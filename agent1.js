1fe306396a81411a191ccd8be66f6673f950e5e4
Shane Watters <shane.watters.nz@gmail.com>
2017-05-26 14:19:09 +1200

    initial expreiment with grid simulation - creating a simulation that determines whether or not a feijoa tree will provide plentiful fruit

--- agent1.js
+++ agent1.js
@@ -5,7 +5,7 @@ function Agent1() {
   // setup is run when the agent is reset
   // value is a number between 0 and 100
   this.setup = function(value) {
-    if(value > 50) {
+    if(value > 95) {
       this.is_alive = true;
     }
     else {
@@ -20,7 +20,22 @@ function Agent1() {
 
   // decide on your next move based on neighbors (but don't do it yet)
   this.step = function(neighbors) {
-    this.next_alive = false;
+    var num_neighbors_alive = 0;
+    for(var i =0; i < neighbors.length; i++){
+      if(neighbors[i].agent.is_alive){
+        num_neighbors_alive++;
+      }
+    }
+
+    if(num_neighbors_alive > 4 && !this.is_alive){
+      this.next_alive = true;
+    }
+    else if(num_neighbors_alive == 2 && !this.is_alive){
+      this.next_alive = 2;
+    }
+    else {
+      this.next_alive = this.is_alive;
+    }
   }
 
   // execute the next move you decided on
@@ -31,11 +46,30 @@ function Agent1() {
   this.draw = function(size) {
     stroke(0);
     if(this.is_alive) {
-      fill(200, 200, 0);
+      if(this.is_alive == 2){
+        fill(05, 205, 0);
+        drawFruit(size);
+      }
+      else {
+        fill(205, 0, 0);
+        drawTree(size);
+      }
+      
     }
     else {
       fill(0, 0, 30);
+      rect(0, 0, size, size);
     }
-    rect(0, 0, size, size);
+    
   }
+}
+
+function drawFruit(size){
+  ellipse(0 + size/2, 0 + size/2, size, size);
+}
+
+function drawTree(size){
+  rect(0 + size/4, size - size/2, size/2, size/2);
+  triangle(0, size-size/8, size/2, size/4, size, size-size/8);
+  triangle(0+size/8, size-size/2, size/2, 0, size-size/8, size-size/2);
 }
