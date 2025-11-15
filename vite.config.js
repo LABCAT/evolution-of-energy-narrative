5d007f6bb0bd183ea836531e71323da873279638
LABCAT <shane.watters.nz@gmail.com>
2025-11-15 20:12:09 +1300

    Updating vite so that viteStaticCopy plugin is not required.

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,5 @@
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/evolution-of-energy-narrative/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        { src: 'agent1.js', dest: '.' },
-        { src: 'agent2.js', dest: '.' },
-        { src: 'agent3.js', dest: '.' },
-        { src: 'sketch.js', dest: '.' },
-        { src: '.purview_helper.js', dest: '.' },
-        { src: 'z_background.jpg', dest: '.' },
-        { src: 'preview.jpg', dest: '.' },
-        { src: 'thumbnail.png', dest: '.' },
-        { src: '.nojekyll', dest: '.' }
-      ]
-    })
-  ]
+  base: '/evolution-of-energy-narrative/'
 });
